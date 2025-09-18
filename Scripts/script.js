d714b5a10e582b577093ca06620856ea3f6f67d2
Silvana <ISS.Design.Team@gmail.com>
2025-09-18 12:13:39 +0300

    Update script.js

---
+++ Scripts/script.js
@@ -0,0 +1,6 @@
+const searchIcon = document.getElementById('searchIcon');
+if (searchIcon) {
+    searchIcon.addEventListener('click', function() {
+        console.log('Search icon clicked');
+    });
+}
